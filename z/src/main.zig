d99bfca358e4c462e6d87e5cfb8034ba9b01c199
Andrew Thomas <andrew@andrewrthomas.com>
2024-02-03 09:36:10 -0800

    silly silly zig compiler

--- z/src/main.zig
+++ z/src/main.zig
@@ -31,51 +31,30 @@ test "simple test" {
 const size: usize = 1024;
 const window_size: usize = size / 2;
 
-fn build_lags(comptime W: i32) @Vector(W, i32) {
-    var lags_arr: [W]i32 = undefined;
-    for (0..W) |i| {
-        lags_arr[i] = i;
-    }
-    const vec: @Vector(W, i32) = lags_arr;
-    return vec;
-}
-
-const lags = build_lags(window_size);
-
 fn detect_pitch(signal: [size]f32) f32 {
-    const signal_vec: @Vector(size, f32) = signal;
-    const diff = diff_fn(0, signal_vec);
-    _ = diff;
-
     // // TODO see if we can SIMD
     // // TODO only use one array if possible
-    // var diffs: [window_size]f32 = undefined;
-    // for (0..window_size) |lag| {
-    //     diffs[lag] = diff_fn(lag, signal);
-    // }
+    var diffs: [window_size]f32 = undefined;
+    for (0..window_size) |lag| {
+        diffs[lag] = diff_fn(lag, signal);
+    }
 
-    // var cmndfs: [window_size]f32 = undefined;
-    // for (0..window_size) |lag| {
-    //     cmndfs[lag] = cmndf(lag, diffs);
-    // }
+    var cmndfs: [window_size]f32 = undefined;
+    for (0..window_size) |lag| {
+        cmndfs[lag] = cmndf(lag, diffs);
+    }
 
     return 0.0;
 }
 
-fn diff_fn(lag: i32, signal_vec: @Vector(size, f32)) f32 {
-    const x = @shuffle(f32, signal_vec, undefined, lags);
-    _ = x;
-    const lag_splat: @Vector(window_size, i32) = @splat(@as(i32, lag));
-    const mask_x_lag = lags + lag_splat;
-    const x_lag = @shuffle(f32, signal_vec, undefined, mask_x_lag);
-    _ = x_lag;
-    // const x: @Vector(window_size, f32) = signal[0..window_size].*;
-    // var x_lag_arr: [window_size]f32 = undefined;
-    // x_lag_arr[0] = signal[lag..(lag + window_size)];
-    // const x_lag: @Vector(window_size, f32) = x_lag_arr;
-    // const diff = x - x_lag;
-    // return @reduce(.Add, diff * diff);
-    return 0.0;
+fn diff_fn(lag: usize, signal: [size]f32) f32 {
+    const x: @Vector(window_size, f32) = signal[0..window_size].*;
+    // this is a little silly, i feel like the compiler could be smarter
+    const x_lag: @Vector(window_size, f32) = signal[lag..(lag + window_size)][0..window_size].*;
+
+    const diff = x - x_lag;
+
+    return @reduce(.Add, diff * diff);
 }
 
 fn cmndf(comptime lag: usize, diffs: [window_size]f32) f32 {
