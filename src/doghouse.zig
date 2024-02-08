a20d347e981a549830a37e600314763a342da9b6
Andrew Thomas <andrew@andrewrthomas.com>
2024-02-08 14:19:14 -0800

    hmmmm

--- src/doghouse.zig
+++ src/doghouse.zig
@@ -2,16 +2,16 @@ const std = @import("std");
 
 // TODO think about padding
 // TODO we might need a min
-pub fn Yin(comptime size: usize, comptime thresh: f32) type {
+pub fn Yin(comptime size: usize, comptime thresh: f32, comptime lb: usize) type {
     const window_size = size / 2;
 
     return struct {
         pub fn detect_pitch(signal: [size]f32, sample_rate: usize) f32 {
             // // TODO see if we can SIMD
             // // TODO only use one array if possible
-            var diffs: [window_size]f32 = undefined;
-            for (0..window_size) |lag| {
-                diffs[lag] = diff_fn(lag, signal);
+            var diffs: [window_size - lb]f32 = undefined;
+            for (0..(window_size - lb)) |lag| {
+                diffs[lag] = diff_fn(lag + lb, signal);
             }
 
             var sample: usize = undefined;
@@ -20,15 +20,15 @@ pub fn Yin(comptime size: usize, comptime thresh: f32) type {
 
             // I don't think this for loop is going away,
             // but we might be able to get rid of the rest
-            for (0..window_size) |lag| {
+            for (0..(window_size - lb)) |lag| {
                 const cmndf_val = cmndf(lag, diffs);
                 if (cmndf_val < thresh) {
-                    sample = lag;
+                    sample = lag + lb;
                     break;
                 } else {
                     if (cmndf_val < min) {
                         min = cmndf_val;
-                        arg_min = lag;
+                        arg_min = lag + lb;
                     }
                 }
             }
@@ -39,16 +39,16 @@ pub fn Yin(comptime size: usize, comptime thresh: f32) type {
         }
 
         fn diff_fn(lag: usize, signal: [size]f32) f32 {
-            const x: @Vector(window_size, f32) = signal[0..window_size].*;
+            const x: @Vector(window_size - lb, f32) = signal[lb..window_size].*;
             // this is a little silly, i feel like the compiler could be smarter
-            const x_lag: @Vector(window_size, f32) = signal[lag..(lag + window_size)][0..window_size].*;
+            const x_lag: @Vector(window_size - lb, f32) = signal[lag..(lag + window_size)][lb..window_size].*;
 
             const diff = x - x_lag;
 
             return @reduce(.Add, diff * diff);
         }
 
-        fn cmndf(lag: usize, diffs: [window_size]f32) f32 {
+        fn cmndf(lag: usize, diffs: [window_size - lb]f32) f32 {
             if (lag == 0) return 1;
 
             // TODO we can SIMD this somehow, I just know it
@@ -68,10 +68,10 @@ test "simple test" {
 
     for (0..1024) |i| {
         const float_idx: f32 = @floatFromInt(i);
-        const val: f32 = @sin(float_idx * sec_per_samp * std.math.pi * 2 * 100);
+        const val: f32 = @sin(float_idx * sec_per_samp * std.math.pi * 2 * 440);
         note[i] = val;
     }
-    const yin = Yin(1024, 0.1);
+    const yin = Yin(1024, 0.01);
     const pitch = yin.detect_pitch(note, 44100);
-    try std.testing.expect(std.math.approxEqAbs(f64, pitch, 100, 10));
+    try std.testing.expect(std.math.approxEqAbs(f64, pitch, 440, 5));
 }
